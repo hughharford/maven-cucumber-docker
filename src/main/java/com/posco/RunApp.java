0e8369a2a74bd25985e93140c59f354363db703d
hsth <hsth@hsthYOGA-13IKB>
2021-01-26 12:10:20 +0000

    Initial commit of Maven, Cucumber & Docker project
    Can build, test and run this minor project

---
+++ src/main/java/com/posco/RunApp.java
@@ -0,0 +1,10 @@
+package com.posco;
+
+public class RunApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("run it, run it, run it!");
+	}
+
+}
